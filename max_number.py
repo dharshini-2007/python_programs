22a72d979846ad029ca0a93b761670a1ea981ea9
dharshini-2007 <dharsh12042007@gmail.com>
2026-07-12 19:01:42 +0530

    finding maximum number

---
+++ max_number.py
@@ -0,0 +1,10 @@
+numbers = [ 3, 17, 8, 25, 9, 12]
+
+max_number = numbers[0]
+
+for num in numbers:
+    if num > max_number:
+        max_number = num
+print("The maximum number is ", max_number)
+            
+
